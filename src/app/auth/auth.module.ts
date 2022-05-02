93f7099e90c07d9a3a0203603344ca53373edf70
IkedaHarket <sebaaignacio111@gmail.com>
2022-05-02 19:25:07 -0400

    componentes auth creados

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    SliderComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
