83a86b5fe22c8e355813d6bd0da0bebe82e24f3f
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-05-05 23:13:05 -0400

    Avance en la page search

--- src/app/shared/components/product-card-full/product-card-full.component.ts
+++ src/app/shared/components/product-card-full/product-card-full.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Producto } from 'src/app/tinpy-public/interfaces/productos/productos-by-name-response';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-product-card-full',
@@ -7,6 +8,8 @@ import { Producto } from 'src/app/tinpy-public/interfaces/productos/productos-by
   styleUrls: ['./product-card-full.component.scss']
 })
 export class ProductCardFullComponent implements OnInit {
+
+  tinpyBackendURL: string = environment.tinpyBackendURL;
 
   @Input()
   producto: Producto = {};
