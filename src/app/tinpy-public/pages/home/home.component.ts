218c967a1cd876b3ffe6e3fbf8fce7598c043cea
sebastian torrealba <sebastian.torrealba@tgtgroup.cl>
2022-10-26 20:20:27 -0300

    Novedades agregado a home

--- src/app/tinpy-public/pages/home/home.component.ts
+++ src/app/tinpy-public/pages/home/home.component.ts
@@ -1,27 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import Swal from 'sweetalert2';
 
 import { ProductosService } from 'src/app/core/services/productos/productos.service';
+import { Producto } from 'src/app/core/interfaces';
+import { map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   formSearch: FormGroup = this.fb.group({
     search: ['',[Validators.required]]
   })
+
+  unProducts!: Subscription;
+  products: Producto[] = [];
 
   constructor(
     private fb:FormBuilder,
     private productosService: ProductosService,
     private router:Router,
     ){}
+
+
+  ngOnInit(): void {
+    this.unProducts = this.productosService.getProductosPaginate().pipe(
+      map(({docs})=> docs),
+      ).subscribe((productos)=> {
+        this.products = productos?.slice(0,6) || [];
+    });
+
+  }
+
   click(){
     if(this.formSearch.invalid) return
     const {search} = this.formSearch.value
@@ -43,5 +59,8 @@ export class HomeComponent {
       },
       error:(err)=> console.log(err),
     });
+  }
+  OnDestroy(){
+    this.unProducts.unsubscribe();
   }
 }
